b008b992163b772489ab94aa8c98637833bf2941
nandhastr <nandhastr2@gmail.com>
2023-11-22 22:52:10 +0700

    add sass style

--- resources/views/manager/Dashboard-manager.blade.php
+++ resources/views/manager/Dashboard-manager.blade.php
@@ -27,9 +27,8 @@
             <!-- Info boxes -->
             <div class="row">
                 <div class="col-12 col-sm-6 col-md-3">
-                    <div class="info-box">
+                    <div class="info-box mr-1">
                         <span class="info-box-icon bg-info elevation-1"><i class="fas fa-cog"></i></span>
-
                         <div class="info-box-content">
                             <span class="info-box-text">Kasir</span>
                             <span class="info-box-number">
@@ -43,7 +42,7 @@
                 </div>
                 <!-- /.col -->
                 <div class="col-12 col-sm-6 col-md-3">
-                    <div class="info-box mb-3">
+                    <div class="info-box mr-1 mb-3">
                         <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-thumbs-up"></i></span>
 
                         <div class="info-box-content">
@@ -60,7 +59,7 @@
                 <div class="clearfix hidden-md-up"></div>
 
                 <div class="col-12 col-sm-6 col-md-3">
-                    <div class="info-box mb-3">
+                    <div class="info-box mr-1 mb-3">
                         <span class="info-box-icon bg-success elevation-1"><i class="fas fa-shopping-cart"></i></span>
 
                         <div class="info-box-content">
@@ -73,7 +72,7 @@
                 </div>
                 <!-- /.col -->
                 <div class="col-12 col-sm-6 col-md-3">
-                    <div class="info-box mb-3">
+                    <div class="info-box mr-1 mb-3">
                         <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-users"></i></span>
 
                         <div class="info-box-content">
