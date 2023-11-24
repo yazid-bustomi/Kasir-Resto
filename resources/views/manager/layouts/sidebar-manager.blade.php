78077623509743745e7e4c9ca82d51a08d182692
Bustomi <bustomi024@gmail.com>
2023-11-25 06:00:22 +0700

    memperbaiki dashboard

--- resources/views/manager/layouts/sidebar-manager.blade.php
+++ resources/views/manager/layouts/sidebar-manager.blade.php
@@ -21,16 +21,23 @@
                 <!-- Add icons to the links using the .nav-icon class
                        with font-awesome or any other icon font library -->
                 <li class="nav-item menu-open">
-                    <a href="#" class="nav-link active">
+                    {{-- <a href="#" class="nav-link active">
                         <i class="nav-icon fas fa-tachometer-alt"></i>
                         <p>
                             Menu
                             <i class="right fas fa-angle-left"></i>
                         </p>
-                    </a>
+                    </a> --}}
                     <ul class="nav nav-treeview">
                         <li class="nav-item pl-2 mr-2">
-                            <a href="{{ route('data-clients') }}" class="nav-link ">
+                            <a href="{{ route('manager.index') }}" class="nav-link">
+                                <i class="nav-icon fas fa-tachometer-alt"></i>
+                                <p>Dashboard</p>
+                            </a>
+                        </li>
+                        
+                        <li class="nav-item pl-2 mr-2">
+                            <a href="{{ route('data.clients') }}" class="nav-link ">
                                 <i class="far fa-user nav-icon"></i>
                                 <p>Data Client</p>
                             </a>
