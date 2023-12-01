7bfb4f96eeb8bc715dbc1711b4a17cd823bc4ab7
intanci <intan.ca1213@gmail.com>
2023-12-01 18:31:34 +0700

    ubah user controller & seederproduk

--- database/seeders/ProdukKategorySeeder.php
+++ database/seeders/ProdukKategorySeeder.php
@@ -14,26 +14,6 @@ class ProdukKategorySeeder extends Seeder
      */
     public function run()
     {
-        $data = [
-            [
-                'id_produk_kategories' => 1,
-                'nama_kategori' => 'Dessert',
-            ],
-            [
-                'id_produk_kategories' => 2,
-                'nama_kategori' => 'Drinks',
-            ],
-            [
-                'id_produk_kategories' => 3,
-                'nama_kategori' => 'Maincourse',
-            ],
-            [
-                'id_produk_kategories' => 4,
-                'nama_kategori' => 'Snack',
-            ],
-            
-        ];
-
-        DB::table('tbl_produk_kategories')->insert($data);
+        
     }
 }
