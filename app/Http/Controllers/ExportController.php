136993520e5f1e42f3ab7973a3cebbf2e41f33a4
Damiri2703 <damiriahmad821@gmail.com>
2023-12-06 17:37:19 +0700

    penambahan export order

---
+++ app/Http/Controllers/ExportController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Export;
+use App\Models\Order;
+use Illuminate\Http\Request;
+
+class ExportController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $orders = Order::all();
+        return view('export.index', [
+            'orders' => $orders,
+            'title' => 'Data Order'
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Export  $export
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Export $export)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Export  $export
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Export $export)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Export  $export
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Export $export)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Export  $export
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Export $export)
+    {
+        //
+    }
+}
